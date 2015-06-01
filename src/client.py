343039282bf0587aa2cb49ece20fc447feb1fbcd
tear44 <tonytear44@gmail.com>
2015-06-01 00:36:44 -0700

    Client and Server updated for multiple clients

--- src/client.py
+++ src/client.py
@@ -5,23 +5,50 @@ import sys
 from threading import Thread
 from time import sleep
 
-
+typeofchat = {'1':'Comment', '2':'Question', '3':'Complaint', '4':'Other'}
 myname = raw_input('What is your name? ')
+# choice = raw_input('How may I help you today? \n1)Comment \n2)Question \n3)Complaint \n4)Other\n')
+# if (choice not in typeofchat):
+#     while (choice not in typeofchat):
+#         choice = raw_input('Please input a valid option. \n1)Comment \n2)Question \n3)Complaint \n4)Other\n')
+# chattopic = raw_input('What is the topic you would like to discuss today?\n')
+
+choice = None
+chattopic = None
+
+topic_chosen_orAgent = False
 
 ## VIEW: class Client handles what the user sees as they chat with an agent
 class Client(Handler):
 
     def on_close(self):
-        print("Connection Closed")
-        sys.exit(0)
+        print('Now exiting!')
+        sys.exit()
 
     def on_msg(self, msg):
         # print msg
-        if (u'txt' in msg):
+        if (u'info' in msg):
+            print(msg[u'info'])
+        elif (u'txt' in msg):
             print("%s: %s"%(msg[u'speak'], msg[u'txt']))
+        elif (u'status' in msg):
+            if (msg[u'status'] == 'Agent'):
+                print("Welcome Agent")
+                global topic_chosen_orAgent
+                topic_chosen_orAgent = True
+            else:
+                print("Welcome Client")
+                # global choice, chattopic
+                # choice = raw_input('How may I help you today? \n1)Comment \n2)Question \n3)Complaint \n4)Other\n')
+                # if (choice not in typeofchat):
+                #     while (choice not in typeofchat):
+                #         choice = raw_input('Please input a valid option. \n1)Comment \n2)Question \n3)Complaint \n4)Other\n')
+                # chattopic = raw_input('What is the topic you would like to discuss today?\n')
+
 
 ## BELOW is Controller
-host, port = 'localhost', 8888
+host, port = '192.168.1.187', 8888
+# host, port = 'localhost', 8888
 client = Client(host, port)
 client.do_send({'join': myname})
 
@@ -36,4 +63,22 @@ thread.start()
 
 while 1:
     mytxt = sys.stdin.readline().rstrip()
-    client.do_send({'speak': myname, 'txt': mytxt})
+    if (topic_chosen_orAgent):
+        if mytxt == ':e':
+            print 'trolololololol'
+        elif mytxt == ':s':
+            pass
+        elif mytxt.lower() == ':q':
+            print('Now exiting!')
+            client.do_send({u'speak': myname, u'txt': "User has left"})
+            sys.exit()
+        else:
+            client.do_send({u'speak': myname, u'txt': mytxt})
+    else:
+        choice = raw_input('How may I help you today? \n1)Comment \n2)Question \n3)Complaint \n4)Other\n')
+        if (choice not in typeofchat):
+            while (choice not in typeofchat):
+                choice = raw_input('Please input a valid option. \n1)Comment \n2)Question \n3)Complaint \n4)Other\n')
+        chattopic = raw_input('What is the topic you would like to discuss today?\n')
+        client.do_send({u'Need': typeofchat[choice], u'text': chattopic})
+        topic_chosen_orAgent = True
--- src/server.py
+++ src/server.py
@@ -10,50 +10,85 @@ from time import sleep
 handlers = {}  # map client handler to user name
 users = {}      # map client name to another client name currently in contact
 Agent = None    #Assume that the first client connected is the agent, store name
-openingLine = {u'speak':'server', u'txt':''''Please enter the number of selected option:\n1) Complaint\t2) Question\t3) Other'''}
+CLIENTS = []    #A python list to hold the clients, current and in line
+
+def next_Client():
+    '''Global function to move and set next client in line'''
+    global CLIENTS, Agent, users, handlers
+    current = CLIENTS.pop(0)
+    users[Agent] = current['name']
+    users[current['name']] = Agent
+    info = 'Now connecting to client %s for %s, on topic "%s"' %(current['name'],
+                                                            current['msg'][u'Need'],
+                                                            current['msg'][u'text'])
+    handlers[Agent].do_send({u'info' : info})
 
 
 # CONTROLLER and Model
 class MyHandler(Handler):
 
     def on_open(self):
-        self.do_send('Connection Accepted')
-        # self.do_send(openingLine)
+        self.do_send('Connecting you with an agent to assist you')
         print('Connection Received')
 
     def on_close(self):
-        self.do_send('Connection Terminated')
-        global users
-        users.pop(self.name)
-        print("Bye %s"%(self.name))
+        self.do_send('Client has left the chat')
+        global users, CLIENTS
+        # users.pop(str(self.name))
+        # print("Bye %s"%(self.name))
+
+        if (len(CLIENTS)):
+            next_Client()
 
 
     def on_msg(self, msg):
         print msg
-        global handlers, users, Agent
-        # self.do_send('received')
+        global handlers, users, Agent, CLIENTS
+
         # CONTROLLER if joined. adds user name to users and handlers
         if (u'join' in msg):
             self.name = msg[u'join']
             handlers[self.name] = handlers["ADD"]
-            print("%s Joined"%(msg[u'join']))
+
             if (not Agent):
                 Agent = self.name
+                users[Agent] = None
+                handlers[self.name].do_send({u'status' : 'Agent'})
+                print("Agent %s has joined"%(msg[u'join']))
             else:
-                users[Agent] = self.name
-                users[self.name] = Agent
+                handlers[self.name].do_send({u'status' : 'Client'})
+                print("%s Joined"%(msg[u'join']))
+                if (users[Agent] == None):
+                    users[Agent] = self.name
+                    users[self.name] = Agent
+                else: #New user is gonna have to wait....
+                    pass
+
+        elif (u'Need' in msg):
+            if (users[Agent] != self.name):
+                CLIENTS.append({'name': self.name, 'msg': msg})
+            else:
+                info = 'Now connecting to client %s for %s, on topic "%s"' %(self.name,
+                                                                        msg[u'Need'],
+                                                                        msg[u'text'])
+                handlers[Agent].do_send({u'info' : info})
+
+            self.do_send({u'info':'You would like option: ' + msg[u'Need'] + '. \nChecking for available agent now..'})
 
         #VIEW and CONTROLLER - based on user input, change view accordingly
         elif (u'txt' in msg):
-            if (msg[u'txt'] == ':q'):
-                self.do_close()
-            else:
-                ## VIEW - displays to correct terminal
+            ## VIEW - displays to correct terminal
+            if (self.name in users):
                 name = users[self.name]
                 if (name in handlers):
+                    if (self.name == Agent):
+                        msg[u'speak'] = 'Agent'
                     handlers[name].do_send(msg)
                 else:
                     pass
+
+
+
 
 #
 class Server(Listener):
